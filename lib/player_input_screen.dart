20aa9480fa6b2a539096e56a3dc7f33aaf3dee4c
Morné le Roux <mornelerx@gmail.com>
2023-02-09 19:42:29 +0200

    updated input text field and refactored docs

--- lib/player_input_screen.dart
+++ lib/player_input_screen.dart
@@ -51,51 +51,63 @@ class _PlayerInputScreenState extends State<PlayerInputScreen> {
                 Hero(
                   tag: "playButton",
 
-//!below is wrapped in a material widget to fix a bug in flutter regarding text and animations
+//! visibilty show and hides the naming input text field
                   child: Visibility(
                     visible: showInput,
-                    child: GestureDetector(
-                      onLongPress: () {
-                        setState(() {
-                          showInput = false;
-                        });
-                      },
-                      child: Material(
-                        color: Colors.transparent,
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                          child: TextField(
-                            onEditingComplete: () {
-                              setState(() {
-                                playerList.add(PlayerWidget(
-                                    playerName: _playerNameController.text));
-                                _playerNameController.clear();
-                              });
-                            },
-                            controller: _playerNameController,
-                            onChanged: (value) {
-                              setState(() {});
-                            },
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Add a player",
-                                hintStyle: GoogleFonts.oswald(
-                                  color: Colors.black26,
-                                )),
+                    child: Column(
+                      children: [
+//! Detect the long press
+                        GestureDetector(
+                          onLongPress: () {
+                            setState(() {
+                              showInput = false; //Hides input
+                            });
+                          },
+
+//!below is wrapped in a material widget to fix a bug in flutter regarding text and animations
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Container(
+                                padding: const EdgeInsets.only(left: 20),
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                child: TextField(
+                                  onEditingComplete: () {
+                                    setState(() {
+                                      playerList.add(PlayerWidget(
+                                          playerName:
+                                              _playerNameController.text));
+                                      _playerNameController.clear();
+                                    });
+                                  },
+                                  controller: _playerNameController,
+                                  onChanged: (value) {
+                                    setState(() {});
+                                  },
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Add a player",
+                                      hintStyle: GoogleFonts.oswald(
+                                        color: Colors.black26,
+                                      )),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Text(
+                          "Long press to hide input",
+                          style: GoogleFonts.oswald(color: Colors.white),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                Text(
-                  "Long press to hide input",
-                  style: GoogleFonts.oswald(color: Colors.white),
-                ),
+
                 Padding(
                     padding: EdgeInsets.only(
                         bottom: MediaQuery.of(context).viewInsets.bottom))
